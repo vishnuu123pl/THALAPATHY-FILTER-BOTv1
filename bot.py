f3500d5d5d87afb07f099b2d05fe33a638d730ac
tejachowdary0 <141829994+tejachowdary0@users.noreply.github.com>
2023-12-23 09:24:58 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -45,7 +45,7 @@ ppath = "plugins/*.py"
 files = glob.glob(ppath)
 LazyPrincessBot.start()
 loop = asyncio.get_event_loop()
-
+loop.create_task(amain(loop=loop))
 
 async def Lazy_start():
     print('\n')
@@ -93,7 +93,7 @@ async def Lazy_start():
 if __name__ == '__main__':
     try:
         
-        loop.run_until_complete(amain(loop=loop))
+        loop.run_until_complete(Lazy_start())
     except KeyboardInterrupt:
         logging.info('Service Stopped Bye 👋')
 
